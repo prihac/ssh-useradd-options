b38a8f8408c8cba660af2121301855cc545b8e23
prihac <prihacker@gmail.com>
2023-03-29 05:13:53 +0300

    Update ssh-useradd-options.sh

--- ssh-useradd-options.sh
+++ ssh-useradd-options.sh
@@ -3,7 +3,7 @@
 echo " ************ "
 # Show Options
 PS3='Please select your choice: '
-options=("Create User" "Delete User" "Update Script" "Quit")
+options=("Create User" "Delete User" "Status User" "Update Script" "Uninstall Script" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -15,10 +15,18 @@ do
             # Run Delete user script
             sh ssh-useradd-options/delete-user.sh
             ;;
+        "Status User")
+            # Run Status user script
+            sh ssh-useradd-options/status-user.sh
+            ;;			
 		"Update Script")
             # Run Update script
-            sh ssh-useradd-options/update-user.sh
+            sh ssh-useradd-options/update-script.sh
             ;;	
+		"Uninstall Script")
+            # Run Uninstall script
+            sh ssh-useradd-options/uninstall-script.sh
+            ;;
         "Quit")
             break
             ;;
