b2769c8f0e99ae82146db317c090fd205e778ea1
prihac <prihacker@gmail.com>
2023-03-29 04:32:24 +0300

    Update delete-user.sh

--- delete-user.sh
+++ delete-user.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Show User Info
+# Show only the last ten users
 echo " ************ "
 echo " Show only the last ten users: "
 cut -d: -f1 /etc/passwd | tail -n 10
 echo " ************ "
 
 # Get username from user
-read -p "Enter username: " username
+read -p "Enter the username you want to delete: " username
 echo
 
 # Delete user
 deluser "${username}"
 
-# Show User Info
+# Show only the last five users
 echo " ************ "
 echo " Show only the last five users: "
 cut -d: -f1 /etc/passwd | tail -n 5
