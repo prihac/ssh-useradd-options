e7989290ad55b52b37e962ede3c39462caeda432
prihac <prihacker@gmail.com>
2023-03-29 07:44:37 +0300

    Update run-script.sh

--- run-script.sh
+++ run-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $1 == "menu" ]]; then
   # Insert your code here
-  . ssh-useradd-options/create-user.sh
-  echo "Running my script..."
+  . ssh-useradd-options/ssh-useradd-options.sh
+  echo "Thank you for Running my script..."
 fi
