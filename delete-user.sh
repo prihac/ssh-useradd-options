34725f5d7c9cc70f18bfc48c7de5bccb11440089
prihac <prihacker@gmail.com>
2023-04-01 03:59:05 +0300

    Update delete-user.sh

--- delete-user.sh
+++ delete-user.sh
@@ -13,13 +13,6 @@ echo
 # Delete user
 deluser "${username}"
 
-if id "${username}" >/dev/null 2>&1; then
-  deluser "${username}"
-else
-  echo "User ${username} does not exist"
-fi
-
-
 # Show only the last five users
 echo " ************ "
 echo " Show only the last five users: "
