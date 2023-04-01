b9f5b14bd62d39c9942555d82209d38dd4fbde91
prihac <prihacker@gmail.com>
2023-04-01 03:53:40 +0300

    Update delete-user.sh

--- delete-user.sh
+++ delete-user.sh
@@ -6,12 +6,12 @@ echo " Show only the last five users: "
 cut -d: -f1 /etc/passwd | tail -n 5
 echo " ************ "
 
-# Get username from user
-read -p "Enter the username you want to delete: " username
-echo
+if id "${username}" >/dev/null 2>&1; then
+  deluser "${username}"
+else
+  echo "User ${username} does not exist"
+fi
 
-# Delete user
-deluser "${username}"
 
 # Show only the last five users
 echo " ************ "
