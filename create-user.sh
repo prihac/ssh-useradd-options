bed58fce18616a7ee4c08ff696b4b3e74ddee584
prihac <prihacker@gmail.com>
2023-04-01 14:09:27 +0300

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -17,10 +17,10 @@ read -p "Enter number of allowed logins: " max_logins
 # Create user with specified options
 useradd -e "${expiration_date}" -f "${expiration_days}" -p "$(openssl passwd -1 "${password}")" -s /bin/false -M -N -l "${username}"
 
-# Get username from user
-read -p "Enter username: " username
-
-# Show User Info
-chage -l "${username}"
+# Show only the last five users
+echo "\e[91m ************ \e[0m"
+echo " Show only the last five users: "
+cut -d: -f1 /etc/passwd | tail -n 5
+echo "\e[91m ************ \e[0m"
 
 exit 0
