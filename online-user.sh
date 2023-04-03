e616f8731999bbe98d3dce5a4de80823c09e95f7
prihac <prihacker@gmail.com>
2023-04-03 03:59:50 +0300

    Update online-user.sh

--- online-user.sh
+++ online-user.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 echo " ************ "
+
+# Check if nethogs is installed
+if ! dpkg -s nethogs >/dev/null 2>&1; then
+    echo "nethogs is not installed, installing it now..."
+    sudo apt-get update
+    sudo apt-get install nethogs -y
+fi
+
 # Online User
 sudo nethogs
+
 echo " ************ "
