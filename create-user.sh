6a90383c3cb527f6101d07785e4d2f39f2bd2676
prihac <prihacker@gmail.com>
2023-04-30 19:08:48 +0300

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -17,6 +17,9 @@ read -p "Enter number of allowed logins: " max_logins
 # Create user with specified options
 useradd -e "${expiration_date}" -f "${expiration_days}" -p "$(openssl passwd -1 "${password}")" -s /bin/false -M -N -l "${username}"
 
+# Modify warning days for password expiration to 100
+chage -W 100 "${username}"
+
 # Show only the last five users
 echo "\e[91m ************ \e[0m"
 echo " Show only the last five users: "
