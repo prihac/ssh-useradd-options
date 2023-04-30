bb3350a425acb5cc86a7aee9578d97d8d3de6fbf
prihac <prihacker@gmail.com>
2023-04-30 19:42:59 +0300

    Update status-user.sh

--- status-user.sh
+++ status-user.sh
@@ -3,7 +3,7 @@
 # Show only the last five users
 echo " ************ "
 echo " Show only the last 100 users: "
-cut -d: -f1 /etc/passwd | tail -n 100
+cut -d: -f1 /etc/passwd | grep -Ev "root|daemon|bin|sys|sync|games|man|lp|mail|news|uucp|proxy|www-data|backup|list|irc|gnats|nobody|_apt|systemd-network|systemd-resolve|messagebus|systemd-timesync|sshd|stunnel4|privoxy" | tail -n 100
 echo " ************ "
 # Get username from user
 read -p "Enter the username you want to Check Information: " username
