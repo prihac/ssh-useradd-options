03a829d7f02c153ff54fab230361bd3cd5577454
prihac <prihacker@gmail.com>
2023-05-14 09:25:41 +0300

    Update mem-info.sh

--- mem-info.sh
+++ mem-info.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define color codes
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # Get total memory
 total_mem=$(free -m | awk '/^Mem:/{print $2}')
 
@@ -9,6 +15,7 @@ used_mem=$(free -m | awk '/^Mem:/{print $3}')
 # Get free memory
 free_mem=$(free -m | awk '/^Mem:/{print $4}')
 
-echo "Total memory: $total_mem MB"
-echo "Used memory: $used_mem MB"
-echo "Free memory: $free_mem MB"
+# Print memory info with colors
+echo -e "${BLUE}Total memory:${NC} ${total_mem} MB"
+echo -e "${GREEN}Used memory:${NC} ${used_mem} MB"
+echo -e "${RED}Free memory:${NC} ${free_mem} MB"
