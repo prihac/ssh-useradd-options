a812c5574d19a006ba322c6397277c5e6d524153
prihac <prihacker@gmail.com>
2023-05-14 05:44:40 +0300

    Update delete-user.sh

--- delete-user.sh
+++ delete-user.sh
@@ -11,6 +11,19 @@ echo "\e[91m ************ \e[0m"
 read -p "Enter the username you want to delete: " username
 echo
 
+# Check if user is currently logged in
+if w | grep -q "$username"; then
+  echo "User is currently logged in. Do you want to force logout and delete the user? (y/n)"
+  read force_logout
+  if [[ "$force_logout" == "y" ]]; then
+    sudo usermod -L "$username"  
+    pkill -KILL -u "$username"
+  else
+    echo "Aborting deletion of user."
+    exit 1
+  fi
+fi
+
 # Delete user
 deluser "${username}"
 echo "\e[91m ************ \e[0m"
