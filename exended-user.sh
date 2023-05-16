406c9a7a9aaf6f44d4877675ec03c67dfdd02934
prihac <prihacker@gmail.com>
2023-05-16 03:23:18 +0300

    Create exended-user.sh

---
+++ exended-user.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get username from user
+read -p "Enter username: " username
+echo
+# Get password from user
+read -p "Enter password: " password
+echo
+
+# Get expiration date from user
+read -p "Enter number of days until account expiration: " expiration_days
+expiration_date=$(date -d "+${expiration_days} days" +%Y-%m-%d)
+
+# Get number of allowed logins from user
+read -p "Enter number of allowed logins: " max_logins
+
+# Create user with specified options
+useradd -e "${expiration_date}" -f "${expiration_days}" -p "$(openssl passwd -1 "${password}")" -s /bin/false -M -N -l "${username}"
+
+# Show only the last five users
+echo "\e[91m ************ \e[0m"
+echo " Show only the last five users: "
+cut -d: -f1 /etc/passwd | tail -n 10
+echo "\e[91m ************ \e[0m"
+
+exit 0
