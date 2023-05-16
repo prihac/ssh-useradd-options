5903ed293124494cf3af59539d1db8e1846acac0
prihac <prihacker@gmail.com>
2023-05-16 06:48:31 +0300

    Create grouply-list-users.sh

---
+++ grouply-actions-users/grouply-list-users.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#Define output file path
+output_file="accounts-grouply-list.txt"
+#Show only the last five users and save to output file
+
+echo " ************ "
+echo " Show only the last 100 users: "
+cut -d: -f1 /etc/passwd | tail -n 100
+echo " ************ "
+
+echo ""
+echo "Show only the last 100 users:"
+cut -d: -f1 /etc/passwd | tail -n 100 > "$output_file"
+echo "User list saved to $output_file"
+echo ""
+
+#exit 0
