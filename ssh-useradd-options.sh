d06238d42f2f1eaf6e9e7aa7b6ac3c82bc8b7ef2
prihac <prihacker@gmail.com>
2023-05-16 06:26:57 +0300

    Update ssh-useradd-options.sh

--- ssh-useradd-options.sh
+++ ssh-useradd-options.sh
@@ -3,13 +3,17 @@
 echo " ************ "
 # Show Options
 PS3='Please select your choice: '
-options=("Create User" "Delete User" "Status User" "Online User" "Update Script" "Uninstall Script" "Quit")
+options=("Create User" "Extend User" "Delete User" "Status User" "Update Script" "Uninstall Script" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Create User")
             # Run Create user script
             sh /root/ssh-useradd-options/create-user.sh
+            ;;
+        "Extend User")
+            # Run Extend user script
+            sh /root/ssh-useradd-options/extended-user.sh
             ;;
         "Delete User")
             # Run Delete user script
@@ -18,22 +22,24 @@ do
         "Status User")
             # Run Status user script
             sh /root/ssh-useradd-options/status-user.sh
-            ;;		
+            ;;
         "Online User")
-            # Run Status user script
+            # Run Online user script
             sh /root/ssh-useradd-options/online-user.sh
-            ;;				
-		"Network Interface")
-			sh /root/ssh-useradd-options/network_interface.sh
-            ;;		
-		"Network Bandwith")
-			sh /root/ssh-useradd-options/network_bandwith.sh
-            ;;		
-		"Update Script")
+            ;;
+        "Network Interface")
+            # Run Network Interface script
+            sh /root/ssh-useradd-options/network_interface.sh
+            ;;
+        "Network Bandwidth")
+            # Run Network Bandwidth script
+            sh /root/ssh-useradd-options/network_bandwidth.sh
+            ;;
+        "Update Script")
             # Run Update script
             sh /root/ssh-useradd-options/update-script.sh
-            ;;	
-		"Uninstall Script")
+            ;;
+        "Uninstall Script")
             # Run Uninstall script
             sh /root/ssh-useradd-options/uninstall-script.sh
             ;;
