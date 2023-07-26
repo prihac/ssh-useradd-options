e4ac1445aed7918578fb8da1dad2203f6c60136a
prihac <prihacker@gmail.com>
2023-07-26 20:49:45 +0300

    Update run_journalctl.sh

--- run_journalctl.sh
+++ run_journalctl.sh
@@ -2,32 +2,39 @@
 
 # Function to execute journalctl for all logs
 execute_journalctl_all() {
-    journalctl _UID=$(id -u $username)
+    journalctl _UID=$(id -u $1)
 }
 
 # Function to execute journalctl for logs with "error"
 execute_journalctl_error() {
-    journalctl _UID=$(id -u $username) | grep "error"
+    journalctl _UID=$(id -u $1) | grep "error"
 }
 
 # Display the menu options
 echo "Menu:"
 echo "1. Execute journalctl for all logs"
 echo "2. Execute journalctl for logs with 'error'"
-read -p "Please enter your choice (1 or 2): " choice
+echo "3. Execute journalctl for multiple usernames"
+read -p "Please enter your choice (1, 2, or 3): " choice
 
-# Prompt the user for the username
-read -p "Please enter the username: " username
-
-# Perform action based on the user's choice
 case $choice in
     1)
-        execute_journalctl_all
+        read -p "Please enter the username: " username
+        execute_journalctl_all $username
         ;;
     2)
-        execute_journalctl_error
+        read -p "Please enter the username: " username
+        execute_journalctl_error $username
+        ;;
+    3)
+        read -p "Please enter the list of usernames separated by spaces: " usernames
+        for user in $usernames; do
+            echo "Journalctl output for user $user:"
+            execute_journalctl_all $user
+            echo "-------------------------------------------------"
+        done
         ;;
     *)
-        echo "Invalid choice. Please enter either 1 or 2."
+        echo "Invalid choice. Please enter either 1, 2, or 3."
         ;;
 esac
