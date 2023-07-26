3cc5e15c2c91f977fac1638a017057edccb83964
prihac <prihacker@gmail.com>
2023-07-26 17:36:19 +0300

    Update run_journalctl.sh

--- run_journalctl.sh
+++ run_journalctl.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+# Function to execute journalctl for all logs
+execute_journalctl_all() {
+    journalctl _UID=$(id -u $username)
+}
+
+# Function to execute journalctl for logs with "error"
+execute_journalctl_error() {
+    journalctl _UID=$(id -u $username) | grep "error"
+}
+
+# Display the menu options
+echo "Menu:"
+echo "1. Execute journalctl for all logs"
+echo "2. Execute journalctl for logs with 'error'"
+read -p "Please enter your choice (1 or 2): " choice
+
 # Prompt the user for the username
 read -p "Please enter the username: " username
 
-# Execute the desired command using journalctl
-journalctl _UID=$(id -u $username)
+# Perform action based on the user's choice
+case $choice in
+    1)
+        execute_journalctl_all
+        ;;
+    2)
+        execute_journalctl_error
+        ;;
+    *)
+        echo "Invalid choice. Please enter either 1 or 2."
+        ;;
+esac
